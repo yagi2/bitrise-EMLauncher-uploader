96c6a1fd22d1a5b18aac1a48c728638e7a7ab0e5
Itsuki Aoyagi <mail@yagi2.com>
2017-06-12 12:40:44 +0900

    implementation upload method with shell script

--- step.sh
+++ step.sh
@@ -5,3 +5,10 @@ echo "api_key   : ${api_key}"
 echo "file_path : ${file_path}"
 echo "title     : ${title}"
 echo "tags      : ${tags}"
+
+curl ${base_url}/api/upload \
+    -F api_key=${api_key} \
+    -F file=@${file_path} \
+    -F title=${title} \
+    -F tags=${tags} \
+    -F "$(printf "description=%s" "$(date)")"
